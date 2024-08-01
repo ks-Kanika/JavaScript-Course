731502e48b896a4ca79575aaee7a5ccc9c73941b
ks-Kanika <159553456+ks-Kanika@users.noreply.github.com>
2024-08-01 07:40:50 +0000

    Datatypes

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat all JS code as new version
+/*   number , bigint , string, boolean, null(standalone value, representation of empty value),
+ undefined(value not defined), symbol, object */
+// console.log(typeof)
+// console.log(typeof null) is a object
+// console.log(typeof undefined) is undeifned bcoz it is a type in jS
+
