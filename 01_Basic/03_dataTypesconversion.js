89b42d23563ec638e8bf43734d55882cc1c37d61
ks-Kanika <159553456+ks-Kanika@users.noreply.github.com>
2024-08-01 08:17:11 +0000

    Conversion of DataTypes

---
+++ 01_Basic/03_dataTypesconversion.js
@@ -0,0 +1,24 @@
+// let score=33
+// let age='51a'  // if it is null , undefiend , true, false
+// console.log(typeof score);
+// console.log(typeof age);
+
+// let valueInNumber= Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber) // if null output is 0 , NAN , 1, 0
+/* 
+"33"=>33
+"33abc"=>NAN
+ true=1
+ false=0
+ "" = in boolean it gives output false
+ "hii" =  adding text or value in boolean it gives output true
+*/
+let isLoggedIn= 1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let num=15;
+let stringNumber=String(num)
+console.log(stringNumber)
+console.log( typeof stringNumber)
