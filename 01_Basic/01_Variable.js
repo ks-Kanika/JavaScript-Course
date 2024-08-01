a1a99166ed1133ccb5d0f247932ffe7cf34cf059
ks-Kanika <159553456+ks-Kanika@users.noreply.github.com>
2024-08-01 07:11:51 +0000

    Config Commit Changes

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,16 @@
+const accountID=23;
+let accountName= "Kanika";
+var accountCity= "Rishikesh";
+accountEmail="kanika@gmail.com" //although not good but it is possible without declaring any variable
+let accountState; /*variable declare but value/output is undefined*/
+
+// let accountID=45;
+ accountName= "Kartik";
+ accountCity= "Kotdwar";
+ accountEmail="kartik@gmail.com"
+ /* Variables : used to store data act as a container
+Let, Const and Var
+Var (does not prefer because of issues in block scope and functional scope) */
+
+console.log(accountID);
+console.table([accountID, accountName, accountCity, accountEmail, accountState])
